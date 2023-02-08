fdb0121055f5c4a02b4ee561d2b905e0ba115934
David Ramos <davidramos015@gmail.com>
2023-02-08 00:53:27 -0600

    Add format numbers helper

---
+++ src/utils/formatNumber.ts
@@ -0,0 +1,54 @@
+import numeral from 'numeral';
+
+if (numeral.locales[ 'en' ] === undefined) {
+    numeral.register('locale', 'en', {
+        delimiters: {
+            thousands: ',',
+            decimal: '.',
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'm',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal: function (num: number) {
+            return num === 1 ? 'st' : num === 2 ? 'nd' : num === 3 ? 'rd' : 'th';
+        },
+        currency: {
+            symbol: '$.'
+        }
+    });
+}
+
+numeral.locale('en');
+
+export function fCurrency(number: string | number) {
+    return numeral(number).format(Number.isInteger(number) ? '$0,0' : '$0,0.00');
+}
+
+export function fCurrencyCap(number: string | number, upper: boolean = true) {
+
+    const resp = numeral(number).format('($ 0.00 a)');
+    return upper ? resp.toUpperCase() : resp;
+}
+
+export function fPercent(number: number) {
+    return numeral(number / 100).format('0.0%');
+}
+
+export function fNumber(number: string | number) {
+    return numeral(number).format();
+}
+
+export function fNumberFloat(number: string | number, fomat: string = '0,0.00') {
+    return numeral(number).format(fomat);
+}
+
+export function fShortenNumber(number: string | number) {
+    return numeral(number).format('0.00a');
+}
+
+export function fData(number: string | number) {
+    return numeral(number).format('0.0 b');
+}
