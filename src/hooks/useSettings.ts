61050d09e54fdaf32f22e39308a8347b6cd25784
David Ramos <davidramos015@gmail.com>
2023-02-08 00:51:12 -0600

    Configure chart and circulating supply

--- src/hooks/useSettings.ts
+++ src/hooks/useSettings.ts
@@ -2,6 +2,4 @@ import { useContext } from 'react';
 import { SettingsContext } from 'src/contexts/SettingsContext';
 
 const useSettings = () => useContext(SettingsContext);
-console.info("useSettings", useSettings);
-
 export default useSettings;
