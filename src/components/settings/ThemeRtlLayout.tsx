b5ff4d4be3e0c13047b48fb5b83cd6b4df986494
David Ramos <davidramos015@gmail.com>
2023-02-10 21:59:32 -0600

    Define app settings and theme

---
+++ src/components/settings/ThemeRtlLayout.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { prefixer } from 'stylis';
+import rtlPlugin from 'stylis-plugin-rtl';
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import { useTheme } from '@mui/material/styles';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function ThemeRtlLayout({ children }: Props) {
+  const theme = useTheme();
+
+  useEffect(() => {
+    document.dir = theme.direction;
+  }, [theme.direction]);
+
+  const cacheRtl = createCache({
+    key: theme.direction === 'rtl' ? 'rtl' : 'css',
+    stylisPlugins: theme.direction === 'rtl' ? [prefixer, rtlPlugin] : [],
+  });
+
+  return <CacheProvider value={cacheRtl}>{children}</CacheProvider>;
+}
---
+++ src/components/settings/config-setting.ts
@@ -0,0 +1,11 @@
+import { SettingsValueProps } from './types';
+
+export const defaultSettings: SettingsValueProps = {
+  themeMode: 'light',
+  themeDirection: 'ltr',
+  themeContrast: 'bold',
+  themeLayout: 'vertical',
+  themeColorPresets: 'orange',
+  themeStretch: false,
+  language: 'en',
+};
