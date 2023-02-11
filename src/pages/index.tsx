63687ab1d99e867c86ecbf907fbf7edc5aaf36e2
David Ramos <davidramos015@gmail.com>
2023-02-10 22:04:06 -0600

    Include menu layouts

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { PATH_DASHBOARD } from "src/routes/paths";
+
+export default function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.pathname === '/') {
+      router.push(PATH_DASHBOARD.root);
+    }
+  });
+
+  return null;
+}
