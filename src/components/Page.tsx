06678d6c726f1d78c0bc5b696303c92aacb1f278
David Ramos <davidramos015@gmail.com>
2023-02-10 22:05:57 -0600

    Add page wrapper

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,29 +1,50 @@
 import { forwardRef, ReactNode } from 'react';
-import { Helmet } from 'react-helmet-async';
-import { Box, BoxProps } from '@mui/material';
+import Head from 'next/head';
+import { Box, BoxProps, Container, ContainerProps } from '@mui/material';
+
+import DashboardLayout from 'src/layouts/dashboard'
+import CompactLayout from 'src/layouts/compact'
+
+import { useSettingsContext } from 'src/contexts/SettingsContext';
 
 interface Props extends BoxProps {
   children: ReactNode;
   meta?: ReactNode;
+  containerSx?: ContainerProps;
   title: string;
 }
 
-const Page = forwardRef<HTMLDivElement, Props>(({ children, title = '', meta, ...other }, ref) => (
-  <>
-    <Helmet>
-      <title>{`${title} | Kitty App v.23.02.08.0250`}</title>
-      {meta}
-    </Helmet>
+const Page = forwardRef<HTMLDivElement, Props>(({ children, title = '', meta, containerSx, ...other }, ref) => {
 
-    <Box
-      ref={ref}
-      paddingLeft={6}
-      paddingRight={6}
-      {...other}
-    >
-      {children}
-    </Box>
-  </>
-));
+  const { themeStretch } = useSettingsContext();
+
+  return (
+    <>
+      <Head>
+        <title>{`${title} | Kitty App v.23.02.08.0250`}</title>
+        {meta}
+      </Head>
+
+      <Container
+        {...containerSx}
+        sx={{
+          ...containerSx?.sx,
+          paddingTop: 6,
+        }}
+        maxWidth={themeStretch ? false : 'xl'}
+      >
+        <Box
+          ref={ref}
+          paddingLeft={6}
+          paddingRight={6}
+          {...other}
+        >
+          {children}
+        </Box>
+      </Container>
+    </>
+  )
+});
 
 export default Page;
+export { DashboardLayout, CompactLayout };
