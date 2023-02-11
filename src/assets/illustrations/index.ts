93e89cfd727deb757befcbb9312ece9467498622
David Ramos <davidramos015@gmail.com>
2023-02-10 21:57:13 -0600

    Add ilustrations

---
+++ src/assets/illustrations/index.ts
@@ -0,0 +1,5 @@
+export { default as ForbiddenIllustration } from './ForbiddenIllustration';
+export { default as SeverErrorIllustration } from './SeverErrorIllustration';
+export { default as MaintenanceIllustration } from './MaintenanceIllustration';
+export { default as PageNotFoundIllustration } from './PageNotFoundIllustration';
+export { default as BackgroundIllustration } from './BackgroundIllustration';
