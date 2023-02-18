fd4647bc36b7dd04f05246051c45acaa99c8006f
David Ramos <davidramos015@gmail.com>
2023-02-18 00:46:38 -0600

    set up gh-pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 module.exports = {
   swcMinify: false,
   trailingSlash: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   env: {
-    // HOST
-    REACT_APP_CCM_API: 'http://localhost:5001/graphql/',
+    REACT_APP_CCM_API: String(process.env.REACT_APP_CCM_API || ''),
   },
 };
